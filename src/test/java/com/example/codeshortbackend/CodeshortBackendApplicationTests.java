18f5c77f482722e7261aeca765a18adde0a8f84d
Tan <75690349+Tann-dev@users.noreply.github.com>
2023-03-17 14:40:28 +0100

    [+] second test github action

--- src/test/java/com/example/codeshortbackend/CodeshortBackendApplicationTests.java
+++ src/test/java/com/example/codeshortbackend/CodeshortBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CodeshortBackendApplicationTests {
 
-    @Test
+    /*@Test
     void contextLoads() {
-    }
+    }*/
 
 }
