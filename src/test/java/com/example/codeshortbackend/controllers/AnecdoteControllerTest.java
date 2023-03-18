a32a6295cac80cd00cd95c896db8d65210d65ac7
mathis <mathistanghe@hotmail.fr>
2023-03-18 17:03:35 +0100

    [ ! ] fix controllers test decorators

--- src/test/java/com/example/codeshortbackend/controllers/AnecdoteControllerTest.java
+++ src/test/java/com/example/codeshortbackend/controllers/AnecdoteControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -24,8 +25,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-@DataJpaTest
-@AutoConfigureTestDatabase( replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AnecdoteControllerTest {
     private AuthenticationService authenticationService;
     private AnecdoteService anecdoteService;
